9bdb824b0be1fff41bbbf7795c00e2f28fd5437c
tims23 <niklas.sassmannshausen@gmx.de>
2024-03-21 20:51:28 +0100

    added dynamic linking

--- start_fellowship/src/pages/startups-list/index.js
+++ start_fellowship/src/pages/startups-list/index.js
@@ -1,6 +1,7 @@
 // ** MUI Imports
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import getDocument from 'src/firebase/firestore/getData'
 import getStartups from 'src/firebase/firestore/getStartups'
@@ -13,15 +14,15 @@ const StartUps = () => {
 
   useEffect(() => {
     getStartups().then((querySnapshot) => {
-      const newData = querySnapshot.result.docs.map((doc) => ({...doc.data(), id:doc.id }));
-      
-      setStartups(newData)
-      console.log(newData)
+      try {
+        const newData = querySnapshot.result.docs.map((doc) => ({...doc.data(), id:doc.id }));
+        setStartups(newData)
+      } catch (error) { }      
     })
   }, [])
   
   const listItems = startups.map((startup) =>
-    <Grid item xs={12} sm={6} md={4}>
+    <Grid item xs={12} sm={6} md={4} key={startup.id}>
       <CardStartUp name={startup.name} image="/images/cards/glass-house.png" description={startup.productDescription}></CardStartUp>
     </Grid>
   );
@@ -29,7 +30,7 @@ const StartUps = () => {
   return (
     <Grid container spacing={6}>
       <Grid item xs={12} sx={{ paddingBottom: 0 }}>
-        <Typography variant='h5'>Startups</Typography>
+        <Typography variant='h5'>StartUps</Typography>
       </Grid>
      
       {listItems}
