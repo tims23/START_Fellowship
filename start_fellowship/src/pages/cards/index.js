4b61a3d7a0301fd4aff84d24e4febbbc619eb3c6
reindlml <reindlmeierluis@icloud.com>
2024-03-21 15:17:13 +0100

    remove cards (1)

--- start_fellowship/src/pages/cards/index.js
+++ start_fellowship/src/pages/cards/index.js
@@ -6,66 +6,43 @@ import Typography from '@mui/material/Typography'
 import CardUser from 'src/views/cards/CardUser'
 import CardImgTop from 'src/views/cards/CardImgTop'
 import CardMobile from 'src/views/cards/CardMobile'
-import CardSupport from 'src/views/cards/CardSupport'
 import CardTwitter from 'src/views/cards/CardTwitter'
 import CardFacebook from 'src/views/cards/CardFacebook'
 import CardLinkedIn from 'src/views/cards/CardLinkedIn'
 import CardAppleWatch from 'src/views/cards/CardAppleWatch'
 import CardMembership from 'src/views/cards/CardMembership'
-import CardInfluencer from 'src/views/cards/CardInfluencer'
-import CardNavigation from 'src/views/cards/CardNavigation'
 import CardWithCollapse from 'src/views/cards/CardWithCollapse'
-import CardVerticalRatings from 'src/views/cards/CardVerticalRatings'
-import CardNavigationCenter from 'src/views/cards/CardNavigationCenter'
 import CardHorizontalRatings from 'src/views/cards/CardHorizontalRatings'
 
 const CardBasic = () => {
   return (
     <Grid container spacing={6}>
-      <Grid item xs={12} sx={{ paddingBottom: 4 }}>
-        <Typography variant='h5'>Basic Cards</Typography>
+      <Grid item xs={12} sx={{ paddingBottom: 0 }}>
+        <Typography variant='h5'>News</Typography>
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
         <CardImgTop />
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
-        <CardUser />
+        <CardImgTop />
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
-        <CardWithCollapse />
+        <CardImgTop />
       </Grid>
       <Grid item xs={12} sm={6}>
-        <CardMobile />
+        <CardImgTop />
       </Grid>
       <Grid item xs={12} sm={6}>
-        <CardHorizontalRatings />
+        <CardImgTop />
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
         <CardAppleWatch />
       </Grid>
-      <Grid item xs={12} md={8}>
-        <CardMembership />
+      <Grid item xs={12} sm={8}>
+        <CardAppleWatch />
       </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <CardInfluencer />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <CardVerticalRatings />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <CardSupport />
-      </Grid>
-      <Grid item xs={12} sx={{ pb: 4, pt: theme => `${theme.spacing(17.5)} !important` }}>
-        <Typography variant='h5'>Navigation Cards</Typography>
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <CardNavigation />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <CardNavigationCenter />
-      </Grid>
-      <Grid item xs={12} sx={{ pb: 4, pt: theme => `${theme.spacing(17.5)} !important` }}>
-        <Typography variant='h5'>Solid Cards</Typography>
+      <Grid item xs={12} sx={{ pb: 0, pt: theme => `${theme.spacing(17.5)} !important` }}>
+        <Typography variant='h5'>Social Media</Typography>
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
         <CardTwitter />
