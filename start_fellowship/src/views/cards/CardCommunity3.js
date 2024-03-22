a08fc899c3b7e6b1c92d0a09b1c22790d496153b
Stefan Cames <53260033+StevWorks@users.noreply.github.com>
2024-03-22 03:34:31 +0000

    adapted

--- start_fellowship/src/views/cards/CardCommunity3.js
+++ start_fellowship/src/views/cards/CardCommunity3.js
@@ -8,13 +8,13 @@ import CardContent from '@mui/material/CardContent'
 const Card3 = () => {
   return (
     <Card>
-      <CardMedia sx={{ height: '9.375rem' }} image='/images/cards/watch-on-hand.jpg' />
+      <CardMedia sx={{ height: '9.375rem' }} image='https://images.handelsblatt.com/BQSDOseA-5I5/cover/1600/1067/201/0/0/0/0.5/0.5/gastbeitrag.avif' />
       <CardContent sx={{ padding: theme => `${theme.spacing(3, 5.25, 4)} !important` }}>
         <Typography variant='h6' sx={{ marginBottom: 2 }}>
-          Apple Watch
+        Triggering Innovation Growth in Germany
         </Typography>
         <Typography variant='body2'>
-          Card 7
+        Dr. Hendrik Brandis, co-founder of Earlybirdventure, shares an optimistic outlook on Germany's potential for revolutionary technologies. He proposes a no-state-funds-required strategy to leverage state creditworthiness, attracting institutional investor capital and significantly increasing investment volume. This approach could benefit the economy by unlocking growth capital needed for scaling innovations while offering attractive investment alternatives and better returns for the insurance industry.
         </Typography>
       </CardContent>
       <Button variant='contained' sx={{ py: 2.5, width: '100%', borderTopLeftRadius: 0, borderTopRightRadius: 0 }}>
--- start_fellowship/src/views/cards/CardCommunity4.js
+++ start_fellowship/src/views/cards/CardCommunity4.js
@@ -7,14 +7,13 @@ import CardContent from '@mui/material/CardContent'
 const Card4 = () => {
   return (
     <Card>
-      <CardMedia sx={{ height: '14.5625rem' }} image='/images/cards/glass-house.png' />
+      <CardMedia sx={{ height: '14.5625rem' }} image='https://cdn.sanity.io/images/bl383u0v/production/3cc59ab78ea5980edb12799254856cbd8daa247c-4000x2935.jpg?rect=2,0,3996,2935&w=900&h=661&q=80&fit=max&auto=format&dpr=2' />
       <CardContent>
         <Typography variant='h6' sx={{ marginBottom: 2 }}>
-          Text
+        Neuralink: First human received a brane implant
         </Typography>
         <Typography variant='body2'>
-          Dirimlaa1
-        </Typography>
+        Elon Musk's Neuralink claims its first human brain implant was a success, aiming to help people with paralysis control devices with their thoughts. Despite FDA approval for human trials, details are limited with no scientific confirmation. Critics question the company's research ethics and long-term patient safety.        </Typography>
       </CardContent>
     </Card>
   )
